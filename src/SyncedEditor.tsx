a6e929663adab498e576bba0fb8c4627aaccb639
jkvyff <kristianvyff@gmail.com>
2020-02-27 23:37:19 -0800

    swap to sockets from emitter

--- src/SyncedEditor.tsx
+++ src/SyncedEditor.tsx
@@ -1,61 +1,67 @@
-import React, { useMemo, useState, useEffect, useRef } from "react";
+import React, {
+  useMemo,
+  useState,
+  useEffect,
+  useRef,
+  useCallback
+} from "react";
 import { Editor, createEditor, Operation } from "slate";
 import { Slate, Editable, withReact } from "slate-react";
+import { withHistory } from "slate-history";
 import { initialValue } from "./slateInitialValue";
-import Mitt from "mitt";
+import io from "socket.io-client";
 
-const emitter: mitt.Emitter = Mitt();
+const socket = io("http://localhost:4000");
 
 export const SyncedEditor = () => {
-  const editor = useMemo(() => withReact(createEditor()), []);
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
   const [value, setValue] = useState(initialValue);
   const remote = useRef(false);
   const id = useRef(`${Date.now()}`);
-  const emmiterchange = useRef(false);
+  const socketchange = useRef(false);
+
+  const updateOperations = useCallback(
+    (options: any) => {
+      setValue(options);
+      const ops = editor.operations
+        .filter(o => {
+          if (o) {
+            return o.type !== "set_selection";
+          }
+          return false;
+        })
+        .map((o: any) => ({ ...o, data: { source: id.current } }));
+      if (ops && ops.length && !remote.current && !socketchange.current) {
+        socket.emit("new-operations", { editorId: id.current, ops });
+      }
+      socketchange.current = false;
+    },
+    [editor.operations]
+  );
 
   useEffect(() => {
-    (emitter as any).on("*", (type: string, ops: any) => {
-      if (id.current !== type) {
-        remote.current = true;
-        Editor.withoutNormalizing(editor, () => {
-          JSON.parse(ops).forEach((op: Operation) => {
-            editor.apply(op);
+    socket.on(
+      "new-remote-operations",
+      ({ editorId, ops }: { editorId: string; ops: Operation[] }) => {
+        if (id.current !== editorId) {
+          remote.current = true;
+          Editor.withoutNormalizing(editor, () => {
+            ops.forEach((op: Operation) => {
+              editor.apply(op);
+            });
           });
-        });
-        remote.current = false;
-        emmiterchange.current = true;
-        console.log("changed elsewhere");
+          remote.current = false;
+          socketchange.current = true;
+          console.log("changed elsewhere");
+        }
       }
-    });
+    );
   }, [editor]);
 
   return (
     <>
       <div className="editor">
-        <Slate
-          editor={editor}
-          value={value}
-          onChange={options => {
-            setValue(options);
-            const ops = editor.operations
-              .filter(o => {
-                if (o) {
-                  return o.type !== "set_selection";
-                }
-                return false;
-              })
-              .map((o: any) => ({ ...o, data: { source: id.current } }));
-            if (
-              ops &&
-              ops.length &&
-              !remote.current &&
-              !emmiterchange.current
-            ) {
-              emitter.emit(id.current, JSON.stringify(ops));
-            }
-            emmiterchange.current = false;
-          }}
-        >
+        <Slate editor={editor} value={value} onChange={updateOperations}>
           <Editable />
         </Slate>
       </div>
